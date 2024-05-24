ac3b374fa3b01a9e5cd48a44cced66d089994d11
lethanhhung275 <lethanhhung275@gmail.com>
2024-05-25 06:53:59 +0700

    CRUD of todo

---
+++ ToDoList2024/ToDo.swift
@@ -0,0 +1,12 @@
+//
+//  ToDo.swift
+//  ToDoList2024
+//
+//  Created by Lê Hưng on 23/5/24.
+//
+
+import UIKit
+
+class ToDo: NSObject {
+
+}
---
+++ ToDoList2024/ToDoCell.swift
@@ -0,0 +1,23 @@
+//
+//  ToDoCell.swift
+//  ToDoList2024
+//
+//  Created by Lê Hưng on 23/5/24.
+//
+
+import UIKit
+
+class ToDoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
